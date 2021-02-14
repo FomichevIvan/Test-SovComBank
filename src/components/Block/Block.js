9cd2f343afc94a7e53deda1e2b1cad990a19949a
Ivan Fomichev <yonderboy@yandex.ru>
2021-02-15 00:09:12 +0300

    dragNdrop: list instead of block

--- src/components/Block/Block.js
+++ src/components/Block/Block.js
@@ -1,19 +1,33 @@
 import DragHolder from "../DragHolder/DragHolder";
-import Star from "../Star/Star"
+import Star from "../Star/Star";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-
-const Block = ({word}) => {
-    //получаем пропсами данные каждого слова и рендерим
-  return (  
-    <div className="block">
-      <span className="dragHolder"><DragHolder/></span>
-      <span className="word">{word.word}</span>
-      <span className="part">{word.partOfSpeech}</span>
-      <span>{word.definition.join(', ').slice(0,20) + "..."}</span>
-      <span>[{word.tr}]</span>
-      <div><Star id={word.id} isLiked={word.isLiked}/></div>
-    </div>
+const Block = ({ word }) => {
+  //получаем пропсами данные каждого слова и рендерим
+  return (
+    <Draggable>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          
+          className="block"
+        >
+          <span className="dragHolder">
+            <DragHolder  />
+          </span>
+          <span className="word">{word.word}</span>
+          <span className="part">{word.partOfSpeech}</span>
+          <span>{word.definition.join(", ").slice(0, 20) + "..."}</span>
+          <span>[{word.tr}]</span>
+          <div>
+            <Star id={word.id} isLiked={word.isLiked} />
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
-}
- 
+};
+
 export default Block;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,65 @@
 import Block from "../Block/Block";
 import uuid from "react-uuid";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+import DragHolder from "../DragHolder/DragHolder";
+import Star from "../Star/Star";
 
 const List = ({ words }) => {
   // получаем слова и рендерим их мапом
   return (
     // рендрерим каждый блок <Block/>
-    <div className="cont">
-      {words && words.map((word) => <Block key={uuid()} word={word} />)}
-    </div>
+    <DragDropContext onDragEnd={(params) => {
+      console.log(params)
+      const destinationInd = params.destination.index
+      const sourceInd = params.source.index
+      //по индексу destinationInd (удаляем 0 элементов) вставляем массив(развернув), который вернулся от words.splice(sourceIndб 1)  Это элемент с индексом sourceInd. Другими словами, на индекс destinationInd ставим то, что находилось на sourceInd
+      words.splice(destinationInd, 0, ...words.splice(sourceInd, 1))
+    }}>
+      <div className="dropCont">
+        <Droppable droppableId="droppable-1">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="cont"
+            >
+              {words &&
+                words.map((word, ind) => (
+                  <Draggable
+                    key={word.id}
+                    draggableId={"draggable-" + word.id}
+                    index={ind}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps} 
+                        className="block"
+                      >
+                        <span className="dragHolder">
+                          <DragHolder  />
+                        </span>
+                        <span className="word">{word.word}</span>
+                        <span className="part">{word.partOfSpeech}</span>
+                        <span>
+                          {word.definition.join(", ").slice(0, 20) + "..."}
+                        </span>
+                        <span>[{word.tr}]</span>
+                        <div>
+                          <Star id={word.id} isLiked={word.isLiked} />
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 };
 
